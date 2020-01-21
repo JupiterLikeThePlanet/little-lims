7fdbb7d7119ed5ed8d2cbb6fbbc3955381d9c12c
nativestranger <eric.ed.arnold@gmail.com>
2020-01-21 11:19:23 -0500

    factory

--- spec/factories/sample_test_reports.rb
+++ spec/factories/sample_test_reports.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :sample_test_report do
     sample_report { create(:sample_report) }
-    input_units { SampleReportTest::PERCENTAGE }
-    primary_display_units { SampleReportTest::PERCENTAGE }
-    secondary_display_units { SampleReportTest::MILLIGRAMS_PER_GRAM }
+    input_units { UnitConverter::PERCENTAGE }
+    primary_display_units { UnitConverter::PERCENTAGE }
+    secondary_display_units { UnitConverter::MILLIGRAMS_PER_GRAM }
     sample_test { create(:sample_test, sample: sample_report.sample) }
   end
 end
